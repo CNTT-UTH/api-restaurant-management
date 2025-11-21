50d49427c574be72879b09f8d17b8609a803edea
linhhuynhcoding <nhatlinh.6449@gmail.com>
2025-11-21 04:32:18 +0000

    [BE] Setup project with Fastify and Config Prisma

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+// Import the framework and instantiate it
+import envConfig, { API_URL } from '@/config'
+import { errorHandlerPlugin } from '@/plugins/errorHandler.plugins'
+import validatorCompilerPlugin from '@/plugins/validatorCompiler.plugins'
+import fastifyAuth from '@fastify/auth'
+import fastifyCookie from '@fastify/cookie'
+import fastifyHelmet from '@fastify/helmet'
+import fastifySocketIO from 'fastify-socket.io'
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import path from 'path'
+import { createFolder } from '@/utils/helpers'
+
+const fastify = Fastify({
+  logger: false
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    createFolder(path.resolve(envConfig.UPLOAD_FOLDER))
+    const whitelist = ['*']
+    fastify.register(cors, {
+      origin: whitelist, // Cho phép tất cả các domain gọi API
+      credentials: true // Cho phép trình duyệt gửi cookie đến server
+    })
+
+    fastify.register(fastifyAuth, {
+      defaultRelation: 'and'
+    })
+    fastify.register(fastifyHelmet, {
+      crossOriginResourcePolicy: {
+        policy: 'cross-origin'
+      }
+    })
+    fastify.register(fastifyCookie)
+    fastify.register(validatorCompilerPlugin)
+    fastify.register(errorHandlerPlugin)
+    fastify.register(fastifySocketIO, {
+      cors: {
+        origin: envConfig.CLIENT_URL
+      }
+    })
+    await fastify.listen({
+      port: envConfig.PORT,
+      host: envConfig.DOCKER ? '0.0.0.0' : 'localhost'
+    })
+    console.log(`Server đang chạy: ${API_URL}`)
+  } catch (err) {
+    console.log(err)
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
