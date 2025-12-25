090aa3de67ca17330a919a3faa71c622e267983f
linhhuynhcoding <nhatlinh.6449@gmail.com>
2025-12-25 02:26:50 +0000

    fix prisma generate

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import envConfig from "@/config";
-import { PrismaClient } from "../../prisma/prisma/client";
-import { PrismaClientOptions } from "../../prisma/prisma/internal/prismaNamespace";
+import { PrismaClient } from "@/prisma/client";
+import { PrismaClientOptions } from "@/prisma/internal/prismaNamespace";
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const connectionString = envConfig.DATABASE_URL
